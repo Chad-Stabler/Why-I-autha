13ddadb9ee25962d936c468d73de3576aed0ea34
Chad Stabler <stablerpsn@gmail.com>
2022-05-16 13:48:04 -0500

    deleted unnecessary variables in app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 import { redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
-
 const signUpForm = document.getElementById('sign-up');
-const signUpEmail = document.getElementById('sign-up-email');
-const signUpPassword = document.getElementById('sign-up-password');
 
 redirectIfLoggedIn();
 
